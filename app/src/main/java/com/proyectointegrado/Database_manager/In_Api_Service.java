29ad4214cec9cd34a3e23659e5dbfd828224b03e
Adolfo <adolfo.prado96@gmail.com>
2019-04-02 23:38:11 +0200

    Commit inicialq

---
+++ app/src/main/java/com/proyectointegrado/Database_manager/In_Api_Service.java
@@ -0,0 +1,27 @@
+package com.proyectointegrado.Database_manager;
+
+import io.reactivex.Observable;
+
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface In_Api_Service {
+
+    @POST("register")
+    @FormUrlEncoded
+    Observable<String> registerUser(
+            @Field("Nombre") String name_user,
+            @Field("Apellidos") String surname_user,
+            @Field("Email") String email_user,
+            @Field("Contraseña") String pass_user,
+            @Field("Tipo_Usuario") String type_user,
+            @Field("Fecha_Registro") String data_register_user);
+
+
+    @POST("login")
+    @FormUrlEncoded
+    Observable<String> login_user(
+            @Field("email") String email_user,
+            @Field("pass") String pass_user);
+}
